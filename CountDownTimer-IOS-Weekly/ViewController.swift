dd611be5396d7211f360ac150babf09950f551db
Knexis <nanakwamnyantakyi@gmail.com>
2016-02-15 17:56:22 +0000

    Count Down Timer UI Dev - IOS
    Count Down Timer UI Dev - IOS as part of the IOS weekly UI
    implementation - 4th Edition

---
+++ CountDownTimer-IOS-Weekly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountDownTimer-IOS-Weekly
+//
+//  Created by Nana Kwame Nyantakyi on 15/02/2016.
+//  Copyright © 2016 6 lines of Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
